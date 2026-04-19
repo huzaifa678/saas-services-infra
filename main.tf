a6c4b4b6b25c4168221d60c373b134adaf9ddbcf
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-04-20 02:38:29 +0500

    correction: removed kms resources from main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,6 @@
 data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
 
-resource "aws_kms_key" "main" {
-  description             = "Shared KMS key for ${var.cluster_name} encryption"
-  deletion_window_in_days = 30
-  enable_key_rotation     = true
-
-  tags = { Name = "${var.cluster_name}-kms" }
-}
-
-resource "aws_kms_alias" "main" {
-  name          = "alias/${var.cluster_name}"
-  target_key_id = aws_kms_key.main.key_id
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "6.6.0"
